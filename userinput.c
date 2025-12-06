d3ad55519b14284b27def62959a6c9f3ee45fb45
goni715 <halesgoni715149123@gmail.com>
2025-12-06 13:42:31 +0600

    data type limitation

---
+++ userinput.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    // int goni, roll;
+
+    // scanf("%d %d", &goni, &roll);
+    // printf("%d %d", goni, roll);
+
+
+    //input %(percenatge as charcacter)
+    //int a,b;
+    //char p;
+    //scanf("%d%c %d%c", &a,&p, &b, &p);
+    //printf("%d%% %d%%", a,b); //output= 10% 20%
+
+    //int a,b;
+    //scanf("%d%% %d%%", &a,&b); //input= 10% 20%
+    //printf("%d%% %d%%", a,b); //output= 10% 20%
+
+    int a,b;
+    scanf("%da %db", &a,&b); //input = 10a 20b
+    printf("%da %db", a,b); //output= 10a 20b
+    return 0;
+}
