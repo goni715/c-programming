ce6234123137d336fe2a2fa3df06d92f35a7bad4
goni715 <halesgoni715149123@gmail.com>
2025-12-07 12:33:18 +0600

    data type limitations

---
+++ limitations.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    long long int a=1234567891011;
+    //printf("%lld", a);
+
+    //limitations for float
+    double x = 2.234578986435;
+    printf("%lf", x); //output=2.23457
+
+    return 0;
+}
