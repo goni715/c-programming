b59a81c9aed8a1948ef36e4010d9eb31c599d49b
goni715 <halesgoni715149123@gmail.com>
2025-12-08 23:33:20 +0600

    logical operator

---
+++ arithmetic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    //sum
+    // int a=20, b=30;
+    // int sum = a+b;
+    // printf("%d", sum);
+
+    // int a=3, b=20;
+    // float dividedValue = b*1.0/a;
+    // printf("%f", dividedValue);
+
+    // Remainder
+    int a=3, b=20;
+    int mod = b%a;
+    printf("%d", mod); //output=2
+}
---
+++ logical.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    double isLoggedIn = 1.5;
+    int isAdmin = 0;
+
+    printf("Regula User: %.1lf, %d goni",isLoggedIn,isAdmin);
+
+    return 0;
+}
